136f8d24c05435b182ec597ee123bd1d1a791c0e
Bucky <buckcode16@gmail.com>
2025-04-25 17:19:03 +0900

    Update .sh, separate run interval and run all

--- run_all.sh
+++ run_all.sh
@@ -5,4 +5,4 @@ python get_items_sku_stock_parallel.py
 echo "Starting get_order_detail_component_parallel.py..."
 python get_order_detail_component_parallel.py
 
-echo "All tasks completed."
+echo "Run all completed."
---
+++ run_stock.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Starting get_items_sku_stock_parallel.py..."
+python get_items_sku_stock_parallel.py
+
+echo "Run stock completed."
